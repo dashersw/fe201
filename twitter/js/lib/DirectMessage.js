15608d5d9e22c86fe7faf9c158be7f4fbfd2d4f6
dashersw <armagan.amcalar@tart.com.tr>
2011-07-06 16:34:40 +0300

    added DirectMessage value object class.

--- twitter/js/lib/DirectMessage.js
+++ twitter/js/lib/DirectMessage.js
@@ -2,3 +2,14 @@
 // Techinox Commercial License
 //
 // @author Armagan Amcalar <armagan@tart.com.tr>
+
+goog.provide('twitter.DirectMessage');
+goog.require('tart');
+
+twitter.DirectMessage = function(fromId, toId, body) {
+    this.id = tart.getUId();
+    this.fromId = fromId;
+    this.toId = toId;
+    this.body = body;
+    this.time = tart.randomTime();
+}
