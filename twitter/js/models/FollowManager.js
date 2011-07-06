8270d212c6ad2eb3d45fc5d73d16b2c3deb35b00
dashersw <armagan.amcalar@tart.com.tr>
2011-07-06 16:39:29 +0300

    implemented FollowManager class. A method still needs to be implemented.

--- twitter/js/models/FollowManager.js
+++ twitter/js/models/FollowManager.js
@@ -3,11 +3,33 @@
 //
 // @author Armagan Amcalar <armagan@tart.com.tr>
 
-var FollowManager = function() {
+goog.provide('twitter.FollowManager');
+goog.require('twitter.Follow');
+goog.require('twitter.UserModel');
+
+twitter.FollowManager = function() {
 
 };
 
-FollowManager.prototype.createFollow = function(follower, following) {
-    var follow = new Follow(follower.id, following.id);
+twitter.FollowManager.prototype.createFollow = function(follower, following) {
+    var follow = new twitter.Follow(follower.id, following.id);
     localStorage.setObject(follow.id, follow);
+}
+
+twitter.FollowManager.prototype.getFollowingByUserId = function(userId) {
+    var followedUsers = [],
+        userModel = new twitter.UserModel();
+
+    for (var follow in localStorage) {
+        follow = localStorage.getObject(follow);
+        if (follow && follow.followerId == userId) {
+            var followedUser = userModel.getUserById(follow.followingId);
+            followedUsers.push(followedUser);
+        }
+    }
+    return followedUsers;
+}
+
+twitter.FollowManager.prototype.getFollowersByUserId = function(userId) {
+
 }
