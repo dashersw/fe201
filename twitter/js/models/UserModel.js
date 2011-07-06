119695ab9b367260260f55cc4a55bf8033c97778
dashersw <armagan.amcalar@tart.com.tr>
2011-07-06 16:39:06 +0300

    implemented UserModel class.

--- twitter/js/models/UserModel.js
+++ twitter/js/models/UserModel.js
@@ -3,11 +3,41 @@
 //
 // @author Armagan Amcalar <armagan@tart.com.tr>
 
-var UserModel = function() {
+goog.provide('twitter.UserModel');
+goog.require('twitter.User');
+goog.require('twitter.Session');
+
+twitter.UserModel = function() {
 
 };
 
-UserModel.prototype.createUser = function(username, password, email) {
-    var user = new User(username, password, email);
+twitter.UserModel.prototype.getUserById = function(userId) {
+    return localStorage.getObject(userId);
+}
+
+twitter.UserModel.prototype.createUser = function(username, password, email) {
+    var user = new twitter.User(username, password, email);
     localStorage.setObject(user.id, user);
+    return user;
+}
+
+twitter.UserModel.prototype.getUserByUsername = function(username) {
+    for (var user in localStorage) {
+        user = localStorage.getObject(user);
+        if (user && user.username && user.username == username) {
+            return user;
+        }
+    }
+}
+
+twitter.UserModel.prototype.login = function(username, password) {
+    for (var user in localStorage) {
+        user = localStorage.getObject(user);
+        if (user && user.username && user.password && user.username == username && user.password == password) {
+            var session = new twitter.Session(user.id);
+            localStorage.setObject("session", session);
+            return true;
+        }
+    }
+    return false;
 }
